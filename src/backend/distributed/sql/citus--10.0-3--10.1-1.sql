f521eb48867b7ba7d8580dbc1e182b3caa48e2e3
Nils Dijk <nils@citusdata.com>
2021-03-23 16:18:08 +0100

    implement online alter database owner propagation

--- src/backend/distributed/sql/citus--10.0-3--10.1-1.sql
+++ src/backend/distributed/sql/citus--10.0-3--10.1-1.sql
@@ -1,3 +1,12 @@
 -- citus--10.0-3--10.1-1
 
+-- add the current database to the distributed objects if not already in there.
+-- this is to reliably propagate some of the alter database commands that might be
+-- supported.
+INSERT INTO citus.pg_dist_object SELECT
+  (SELECT oid FROM pg_class WHERE relname = 'pg_database') AS oid,
+  (SELECT oid FROM pg_database WHERE datname = current_database()) as objid,
+  0 as objsubid
+ON CONFLICT DO NOTHING;
+
 #include "../../columnar/sql/columnar--10.0-3--10.1-1.sql"
